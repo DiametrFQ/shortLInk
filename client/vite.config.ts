74e0c7ef01603bb26c63e46241189a97f7fbb80d
DiametrFQ <hohlov.03@inbox.ru>
2024-02-29 13:20:25 +0300

    prepare mongo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+// require("dotenv").config();
+
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: (process.env.PORT as unknown as number) || 3001,
+    port: (process.env.PORT as unknown as number) || 4000,
   },
   server: {
     watch: {
@@ -13,6 +15,6 @@ export default defineConfig({
     },
     host: true,
     strictPort: true,
-    port: (process.env.PORT as unknown as number) || 3001,
+    port: (process.env.PORT as unknown as number) || 4000,
   },
 });
