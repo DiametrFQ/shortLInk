a7b27f4ec296701b4b444e1a3ba87708e294cd90
DiametrFQ <hohlov.03@inbox.ru>
2024-03-01 13:04:14 +0300

    server tests

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,46 @@
 import express from "express";
+import cors from "cors";
 require("dotenv").config();
 
 const app = express();
-const PORT: number = (process.env.PORT as unknown as number) || 4001;
+const PORT: number = (process.env.PORT as unknown as number) || 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+app.use(cors());
+app.use(express.json());
+
+function randomInteger(min: number, max: number): number {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+function randomChar(): string {
+  return String.fromCharCode(randomInteger(97, 122));
+}
+
+function randomString(length: number): string {
+  let string = "";
+  for (let i = 0; i < length; i++) {
+    string += randomChar();
+  }
+  return string;
+}
+
+//Use for test, DONT TOUCH!!!
+app.get("/", function (_, res) {
+  res.send("Hello World1!");
+});
+
+app.post("/create-link", (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  const { longLink } = req.body;
+
+  const path = randomString(7);
+
+  app.get(`/:${path}`, function (_, res) {
+    res.redirect(longLink);
+  });
+
+  res.send({ shortLink: path });
 });
 
 const server = app.listen(PORT, () =>
--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -1,14 +1,43 @@
-import request from "supertest";
+import request, { Response } from "supertest";
 import app, { server } from "../index";
 
-describe("Test the root path", () => {
-  test("It should response the GET method", (done) => {
-    request(app)
-      .get("/")
-      .then(async (response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      })
-      .finally(() => server.close());
+const testLongLink =
+  "https://www.google.com/search?q=Lorem+ipsum%2C+dolor+sit+amet+consectetur+adipisicing+elit.+Facere+ut+nobis+non+velit!+Adipisci+itaque+libero+quo+earum+dicta+doloremque+illo+in+deserunt.+Facilis+tempora+voluptas+minima+aut+cumque+debitis!&oq=Lorem+ipsum%2C+dolor+sit+amet+consectetur+adipisicing+elit.+Facere+ut+nobis+non+velit!+Adipisci+itaque+libero+quo+earum+dicta+doloremque+illo+in+deserunt.+Facilis+tempora+voluptas+minima+aut+cumque+debitis!&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTIyNzMzajBqN6gCALACAA&sourceid=chrome&ie=UTF-8";
+
+describe("GET /", () => {
+  test('It should respond with "Hello World1!"', async () => {
+    const response = await request(app).get("/");
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Hello World1!");
   });
+});
+
+describe("POST /create-link", () => {
+  let path: Response | undefined;
+
+  test("It should respond with a shortLink", async () => {
+    path = await request(app)
+      .post("/create-link")
+      .send({ longLink: testLongLink });
+
+    expect(path.status).toBe(200);
+    expect(path.body).toHaveProperty("shortLink");
+  });
+
+  test("It should create a redirect for the shortLink", async () => {
+    if (!path) {
+      throw new Error("Path is empty");
+    }
+
+    const shortLink = path.body.shortLink;
+    const redirectResponse = await request(app).get(`/${shortLink}`);
+
+    expect(redirectResponse.status).toBe(302);
+    expect(redirectResponse.header.location).toBe(testLongLink);
+  });
+});
+
+afterAll(() => {
+  server.close();
 });
